600afa6d774c511cd5158001e8069c763daf9678
Mevin Fernando <mevin@madstreetden.com>
2021-05-08 11:46:31 +0530

    updated url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://modest-edison-0abd37.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://reverent-beaver-97b0e6.netlify.app/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
